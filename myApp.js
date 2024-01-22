8eb708b990d1ccae92c06c535884aacb8984522f
LucasVega2004 <luquitasvegablanco@gmail.com>
2024-01-22 21:42:31 +0000

    ej 7

--- myApp.js
+++ myApp.js
@@ -53,8 +53,10 @@ const findOneByFood = (food, done) => {
   });};
 
 const findPersonById = (personId, done) => {
-  done(null /*, data*/);
-};
+  Person.findById({ id: personId }, function (err, personFound) {
+    if (err) return console.log(err);
+    done(null, personFound);
+  });};
 
 const findEditThenSave = (personId, done) => {
   const foodToAdd = "hamburger";
