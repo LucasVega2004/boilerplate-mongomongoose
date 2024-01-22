1142de8124112d660d98ded30d620b1442523e11
LucasVega2004 <luquitasvegablanco@gmail.com>
2024-01-22 21:08:16 +0000

    ej 5

--- myApp.js
+++ myApp.js
@@ -26,7 +26,7 @@ const createAndSavePerson = (done) => {
 };
 
 const createManyPeople = (arrayOfPeople, done) => {
-  
+
 
   Person.create(arrayOfPeople, function (err, people) {
     if (err) {
@@ -40,7 +40,10 @@ const createManyPeople = (arrayOfPeople, done) => {
 };
 
 const findPeopleByName = (personName, done) => {
-  done(null /*, data*/);
+  Person.find({ name: personName }, function (err, personFound) {
+    if (err) return console.log(err);
+    done(null, personFound);
+  });
 };
 
 const findOneByFood = (food, done) => {
