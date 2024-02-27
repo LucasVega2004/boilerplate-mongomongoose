81772b4d6180703bfb4264085b72ed528ac32853
LucasVega2004 <luquitasvegablanco@gmail.com>
2024-02-27 17:54:24 +0000

    7.9

--- myApp.js
+++ myApp.js
@@ -96,13 +96,13 @@ const queryChain = (done) => {
 //----- **DO NOT EDIT BELOW THIS LINE** ----------------------------------
 
 exports.PersonModel = Person;
-exports.createAndSavePerson = createAndSavePerson;
+// exports.createAndSavePerson = createAndSavePerson;
 exports.findPeopleByName = findPeopleByName;
 exports.findOneByFood = findOneByFood;
 exports.findPersonById = findPersonById;
 exports.findEditThenSave = findEditThenSave;
 exports.findAndUpdate = findAndUpdate;
-exports.createManyPeople = createManyPeople;
+// exports.createManyPeople = createManyPeople;
 exports.removeById = removeById;
 exports.removeManyPeople = removeManyPeople;
 exports.queryChain = queryChain;
